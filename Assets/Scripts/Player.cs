f57eb1fd7d5bfbb5d5f2721f062fa67910ab620c
Nepsis <93186075+Nepsis7@users.noreply.github.com>
2023-02-16 12:34:28 +0100

    kb

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using Unity.Netcode;
 using UnityEngine;
 using SF = UnityEngine.SerializeField;
 
+#region Settings Classes
 [Serializable]
 class PlayerInputSettings
 {
@@ -19,7 +20,33 @@ class PlayerInputSettings
     public string PitchAxis => pitchAxis;
 }
 
+[Serializable]
+class HitRayCastSettings
+{
+    [SF] float forwardOffset = .4f;
+    [SF] float length = 2.6f;
+    public float ForwardOffset => forwardOffset;
+    public float Length => length;
+}
 
+[Serializable]
+class IsGroundedRayCastSettings
+{
+    [SF] float heightOffset = -.98f;
+    [SF] float length = .04f;
+    public float HeightOffset => heightOffset;
+    public float Length => length;
+}
+
+[Serializable]
+class KnockBackMultipliers
+{
+    [SF] float vertical = 1f;
+    [SF] float horizontal = 1f;
+    public float Vertical => vertical;
+    public float Horizontal => horizontal;
+}
+#endregion Settings Classes
 
 [RequireComponent(typeof(Rigidbody))]
 public class Player : NetworkBehaviour
@@ -29,17 +56,17 @@ public class Player : NetworkBehaviour
     [SF] float deathHeight = -1f;
     [SF] float speed = 2f;
     [SF] float inAirSpeedFactor = .5f;
-    [SF] Vector2 sensitivity = new Vector2(2f, 2f);
-    [SF] Vector2 camPitchMinMax = new Vector2(-70, 85);
+    [SF] Vector2 sensitivity = new(2f, 2f);
+    [SF] Vector2 camPitchMinMax = new(-70, 85);
     [SF] Transform camSocket = null;
-    [SF] PlayerInputSettings inputSettings = new PlayerInputSettings();
-    [SF, Header("A/Hit Raycast")] float oui = -.98f;
-    [SF, Header("Advanced")] float isGroundedRayCastHeightOffset = -.98f;
-    [SF] float isGroundedRayCastLength = .04f;
+    [SF] PlayerInputSettings inputSettings = new();
+    [SF] IsGroundedRayCastSettings isGroundedRayCastSettings = new();
+    [SF] HitRayCastSettings hitRayCastSettings = new();
+    [SF] KnockBackMultipliers kbMultipliers = new();
     #endregion Serialized
     #region Syncronized
-    NetworkVariable<Vector3> syncedPosition = new NetworkVariable<Vector3>(Vector3.zero);
-    NetworkVariable<float> syncedYaw = new NetworkVariable<float>(0f);
+    NetworkVariable<Vector3> syncedPosition = new(Vector3.zero);
+    NetworkVariable<float> syncedYaw = new(0f);
     #endregion Syncronized
     Rigidbody rb = null;
     float camPitch = 0f;
@@ -54,9 +81,11 @@ public class Player : NetworkBehaviour
     }
     private void Start()
     {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
         if (IsLocalPlayer)
         {
-            RespawnClient(false);
+            Respawn(false);
             InitCam();
         }
     }
@@ -68,6 +97,7 @@ public class Player : NetworkBehaviour
             Move();
             CheckDeath();
             HandleCam();
+            TryHitPlayers();
         }
         else if (IsClient)
         {
@@ -99,7 +129,7 @@ public class Player : NetworkBehaviour
     }
     void HandleCam() //cam position, cam rotation & player rotation
     {
-        transform.eulerAngles += Vector3.up * Input.GetAxis(inputSettings.YawAxis) * sensitivity.x;
+        transform.eulerAngles += Input.GetAxis(inputSettings.YawAxis) * sensitivity.x * Vector3.up;
         Camera _cam = Camera.main;
 
         camPitch += -Input.GetAxis(inputSettings.PitchAxis) * sensitivity.y;
@@ -116,18 +146,42 @@ public class Player : NetworkBehaviour
     void CheckDeath()
     {
         if (transform.position.y <= deathHeight)
-            RespawnClient();
+            Respawn();
     }
-    void RespawnClient(bool _addFall = true)
+    void Respawn(bool _addFall = true)
     {
         if (SpawnManager.Instance)
             transform.position = SpawnManager.Instance.GetSpawnPoint();
+        rb.velocity = Vector3.zero;
         RefreshPositionServerRpc(transform.position);
         if (_addFall)
             AddFallServerRpc();
     }
-    void RefreshIsGrounded() => isGrounded = Physics.Raycast(transform.position + Vector3.up * isGroundedRayCastHeightOffset, -Vector3.up, isGroundedRayCastLength, ~gameObject.layer);
+    void TryHitPlayers()
+    {
+        if (!camSocket || !cam || !Input.GetKeyDown(inputSettings.HitKey))
+            return;
+        Vector3 _rayStartPos = camSocket.position + cam.transform.forward * hitRayCastSettings.ForwardOffset;
+        Vector3 _rayDirection = cam.transform.forward;
+        if (!Physics.Raycast(_rayStartPos, _rayDirection, out RaycastHit _hit, hitRayCastSettings.Length))
+            return;
+        Player _hitPlayer = _hit.collider.GetComponent<Player>();
+        if (!_hitPlayer)
+            return;
+        Vector3 _kbDir = cam.transform.forward;
+        _kbDir.y = 1;
+        HitPlayerServerRpc(_hitPlayer.NetworkObjectId, _kbDir.normalized);
+    }
+    void RefreshIsGrounded() => isGrounded = Physics.Raycast(transform.position + Vector3.up * isGroundedRayCastSettings.HeightOffset, -Vector3.up, isGroundedRayCastSettings.Length, ~gameObject.layer);
     #endregion LocalPlayer
+    #region ClientRpc
+    [ClientRpc]
+    void KnockBackIfLocalPlayerClientRpc(Vector3 _dirNormalized)
+    {
+        if (IsLocalPlayer)
+            rb.AddForce(4*new Vector3(_dirNormalized.x * kbMultipliers.Horizontal, _dirNormalized.y * kbMultipliers.Vertical, _dirNormalized.z * kbMultipliers.Horizontal), ForceMode.Impulse); ;
+    }
+    #endregion ClientRpc
     #region ServerRpc
     [ServerRpc]
     void RefreshPositionServerRpc(Vector3 _position)
@@ -140,9 +194,9 @@ public class Player : NetworkBehaviour
     {
         transform.eulerAngles = Vector3.up * _yaw;
         syncedYaw.Value = _yaw;
-
     }
     [ServerRpc] void AddFallServerRpc() => DeathBoard.Instance?.AddFallServer(NetworkObjectId);
+    [ServerRpc] void HitPlayerServerRpc(ulong _playerID, Vector3 _dirNormalized) => NetworkManager.SpawnManager.SpawnedObjects[_playerID].GetComponent<Player>().KnockBackIfLocalPlayerClientRpc(_dirNormalized);
     #endregion ServerRpc
     #endregion Methods
 }
