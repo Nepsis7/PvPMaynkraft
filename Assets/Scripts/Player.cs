ea372b5b98ec326b2b0394581b9fc0fc6bc6ca4f
Nepsis <93186075+Nepsis7@users.noreply.github.com>
2023-02-16 10:21:19 +0100

    player input settings

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,34 @@
+using System;
 using Unity.Netcode;
 using UnityEngine;
+using SF = UnityEngine.SerializeField;
+
+[Serializable]
+class PlayerInputSettings
+{
+    [SF] KeyCode hitKey = KeyCode.Mouse0;
+    [SF] string forwardAxis = "Vertical";
+    [SF] string strafeAxis = "Horizontal";
+    [SF] string yawAxis = "Mouse X";
+    [SF] string pitchAxis = "Mouse Y";
+    public KeyCode HitKey => hitKey;
+    public string ForwardAxis => forwardAxis;
+    public string StrafeAxis => strafeAxis;
+    public string YawAxis => yawAxis;
+    public string PitchAxis => pitchAxis;
+}
 
 [RequireComponent(typeof(Rigidbody))]
 public class Player : NetworkBehaviour
 {
     #region Fields
     #region Serialized
-    [SerializeField] float deathHeight = -1f;
-    [SerializeField] float speed = 2f;
-    [SerializeField] Vector2 sensitivity = new Vector2(2f, 2f);
-    [SerializeField] Vector2 camPitchMinMax = new Vector2(-70, 85);
-    [SerializeField] Transform camSocket = null;
+    [SF] float deathHeight = -1f;
+    [SF] float speed = 2f;
+    [SF] Vector2 sensitivity = new Vector2(2f, 2f);
+    [SF] Vector2 camPitchMinMax = new Vector2(-70, 85);
+    [SF] Transform camSocket = null;
+    [SF] PlayerInputSettings inputSettings = new PlayerInputSettings();
     #endregion Serialized
     #region Syncronized
     NetworkVariable<Vector3> syncedPosition = new NetworkVariable<Vector3>(Vector3.zero);
@@ -50,7 +68,7 @@ public class Player : NetworkBehaviour
     }
     #endregion UnityStuff
     #region LocalPlayer
-    void SetPositionIfLocalPlayer(Vector3 _position)
+    public void SetPositionIfLocalPlayer(Vector3 _position)
     {
         if (!IsLocalPlayer)
             return;
@@ -67,10 +85,10 @@ public class Player : NetworkBehaviour
     }
     void HandleCamClient() //cam position, rotation & player rotation
     {
-        transform.eulerAngles += Vector3.up * Input.GetAxis("Mouse X") * sensitivity.x;
+        transform.eulerAngles += Vector3.up * Input.GetAxis(inputSettings.YawAxis) * sensitivity.x;
         Camera _cam = Camera.main;
 
-        camPitch += -Input.GetAxis("Mouse Y") * sensitivity.y;
+        camPitch += -Input.GetAxis(inputSettings.PitchAxis) * sensitivity.y;
         camPitch = camPitch < camPitchMinMax.x ? camPitchMinMax.x : camPitch;
         camPitch = camPitch > camPitchMinMax.y ? camPitchMinMax.y : camPitch;
         _cam.transform.localEulerAngles = Vector3.right * camPitch;
@@ -78,7 +96,7 @@ public class Player : NetworkBehaviour
     }
     void MoveClient()
     {
-        rb.position += Time.deltaTime * speed * (transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal"));
+        rb.position += Time.deltaTime * speed * (transform.forward * Input.GetAxis(inputSettings.ForwardAxis) + transform.right * Input.GetAxis(inputSettings.StrafeAxis));
         RefreshPositionServerRpc(transform.position);
     }
     void CheckDeathClient()
